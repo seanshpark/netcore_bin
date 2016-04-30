52424145813d6e129e371ec6ff7bda0b68fa1a75
Sung-Jae Lee <sjlee@mail.com>
2016-04-30 22:04:07 +0900

    dotnet-buildall.sh enhancement

--- dotnet-buildall.sh
+++ dotnet-buildall.sh
@@ -1,49 +1,227 @@
-#!/bin/sh
+#!/bin/bash -x
+#
+#   Copyright 2016 by Sung-Jae Lee (sjlee@mail.com)
+#
+
+#
+# set default configuration
+#
+BUILD_ARM=
+BUILD_SOFTFP=
+BUILD_HOST=
+BUILD_MANAGED=
+BUILD_CLI=
+BUILD_ROSLYN=
+BUILD_TYPE=debug
 CLEAN=
+DISTCLEAN=
 SKIPMSCORLIB=
 SKIPTESTS=skiptests
-LOG_FILE=~/git/dotnet_buildall.log
-TIME="time -o $LOG_FILE -a"
-TIME=$(which time)
+
+#
+# etc.
+#
+BASE_PATH=~/git
+LOG_FILE="$BASE_PATH/$(basename ${0}).log"
+#TIME="time -o $LOG_FILE -a"
 TIME="time"
 
-if [ -e $LOG_FILE ];
+#
+# check log file
+#
+if [ -e $LOG_FILE ]
 then
 	rm -f $LOG_FILE
 fi
 
+#
+# parse command-line options
+#
 while [ -n "$1" ]
 do
 	case $1 in
+		all)
+			BUILD_ARM=YES
+			BUILD_SOFTFP=YES
+			BUILD_HOST=YES
+			BUILD_MANAGED=YES
+			BUILD_CLI=YES
+			BUILD_ROSLYN=YES
+			;;
+		arm)
+			BUILD_ARM=YES
+			BUILD_MANAGED=YES
+			;;
+		softfp)
+			BUILD_SOFTFP=YES
+			BUILD_MANAGED=YES
+			;;
+		host)
+			BUILD_HOST=YES
+			BUILD_MANAGED=YES
+			;;
+		CLI)
+			BUILD_CLI=YES
+			;;
+		loslyn)
+			BUILD_ROSLYN=YES
+			;;
+		native-only)
+			BUILD_MANAGED=
+			;;
+		quick)
+			CLEAN=
+			SKIPMSCORLIB=
+			SKIPTESTS=skiptests
+			break
+			;;
 		clean)
-			CLEAN=clean
+			CLEAN=$1
+			;;
+		distclean)
+			DISTCLEAN=YES
+			break
 			;;
 		skipmscorlib)
-			SKIPMSCORLIB=skipmscorlib
+			SKIPMSCORLIB=$1
 			;;
 		skiptests)
-			SKIPTESTS=skiptests
+			SKIPTESTS=$1
+			;;
+		debug|release|checked)
+			BUILD_TYPE=$1
 			;;
 	esac
 	shift
 done
 
-echo "[CORECLR - cross arm]" >> $LOG_FILE
-cd ~/git/coreclr
-ROOTFS_DIR=~/arm-rootfs-coreclr/ $TIME ./build.sh $CLEAN arm cross verbose $SKIPMSCORLIB
-echo "[COREFX - cross arm native]" >> $LOG_FILE
-cd ~/git/corefx
-ROOTFS_DIR=~/arm-rootfs-corefx/ $TIME ./build.sh $CLEAN arm cross verbose native $SKIPTESTS
-$TIME ./build.sh verbose $SKIPTESTS
-echo "[CORECLR - cross arm-softfp]" >> $LOG_FILE
-cd ~/git/coreclr
-ROOTFS_DIR=~/arm-softfp-rootfs-coreclr/ $TIME ./build.sh $CLEAN arm-softfp cross verbose $SKIPMSCORLIB
-echo "[CORECLR]" >> $LOG_FILE
-cd ~/git/coreclr
-$TIME ./build.sh $CLEAN verbose
-echo "[CLI]" >> $LOG_FILE
-cd ~/git/cli
-$TIME ./build.sh
-echo "[ROSLYN]" >> $LOG_FILE
-cd ~/git/roslyn
-$TIME make
+function sync
+{
+	BRANCH=$(git -C $1 branch | grep '*' | cut -d' ' -f2-)
+	UPSTREAM=$(git -C $1 remote | grep -v origin)
+
+	echo ">>>> sync [$1] to upstream <<<<"
+	git -C $1 fetch --all
+	git -C $1 merge $UPSTREAM/$BRANCH
+	git -C $1 push
+	git -C $1 pull --rebase
+	echo
+}
+
+function sync_repo 
+{
+	case $# in
+		0)
+			for repo in $(find $BASE_PATH -maxdepth 1 -type d)
+			do
+				if [ -e $repo/.git ]
+				then
+					sync $repo
+				fi
+			done
+			;;
+		*)
+			while [ -n "$1" ] && [ -e $1/.git ]
+			do
+				sync $1
+				shift
+			done
+			;;
+	esac
+}
+
+function distclean 
+{
+	for repo in $(find $BASE_PATH -maxdepth 1 -type d)
+	do
+		if [ -e $repo/.git ]
+		then
+			git -C $repo clean -xdf
+		fi
+	done
+}
+
+#
+# clean completely
+#
+if [ "$DISTCLEAN" =  "YES" ]
+then
+	distclean
+	exit
+fi
+
+#
+# build arm native
+#
+if [ "$BUILD_ARM" = "YES" ]
+then
+	echo "[CORECLR - cross arm]" >> $LOG_FILE
+	cd $BASE_PATH/coreclr
+	ROOTFS_DIR=~/arm-rootfs-coreclr/ $TIME ./build.sh $BUILD_TYPE $CLEAN arm cross verbose $SKIPTESTS $SKIPMSCORLIB 
+	echo "cross arm build result $?" >> $LOG_FILE
+
+	echo "[COREFX - cross arm native]" >> $LOG_FILE
+	cd $BASE_PATH/corefx
+	ROOTFS_DIR=~/arm-rootfs-corefx/ $TIME ./build.sh native $BUILD_TYPE $CLEAN arm cross verbose $SKIPTESTS
+	echo "cross arm native build result $?" >> $LOG_FILE
+fi
+
+#
+# build arm-softfp native
+#
+if [ "$BUILD_SOFTFP" = "YES" ]
+then
+	echo "[CORECLR - cross arm-softfp]" >> $LOG_FILE
+	cd $BASE_PATH/coreclr
+	ROOTFS_DIR=~/arm-softfp-rootfs-coreclr/ $TIME ./build.sh $BUILD_TYPE $CLEAN arm-softfp cross verbose $SKIPTESTS $SKIPMSCORLIB 
+	echo "cross arm-softfp build result $?" >> $LOG_FILE
+fi
+
+#
+# build host native
+#
+if [ "$BUILD_HOST" = "YES" ]
+then
+	echo "[CORECLR - host]" >> $LOG_FILE
+	cd $BASE_PATH/coreclr
+	$TIME ./build.sh $BUILD_TYPE $CLEAN verbose
+	echo "build result $?" >> $LOG_FILE
+
+	echo "[COREFX - host]" >> $LOG_FILE
+	cd $BASE_PATH/corefx
+	$TIME ./build.sh native $BUILD_TYPE $CLEAN verbose $SKIPTESTS
+	echo "host build result $?" >> $LOG_FILE
+fi
+
+#
+# build managed assembly
+#
+if [ "$BUILD_MANAGED" = "YES" ]
+then
+	echo "[COREFX - managed]" >> $LOG_FILE
+	cd $BASE_PATH/corefx
+	$TIME ./build.sh managed $BUILD_TYPE $CLEAN verbose $SKIPTESTS
+	echo "managed build result $?" >> $LOG_FILE
+fi
+
+#
+# build cli
+#
+if [ "$BUILD_CLI" = "YES" ]
+then
+	echo "[CLI]" >> $LOG_FILE
+	cd $BASE_PATH/cli
+	$TIME ./build.sh $BUILD_TYPE
+	echo "build result $?" >> $LOG_FILE
+fi
+
+#
+# build roslyn
+#
+if [ "$BUILD_ROSLYN" = "YES" ]
+then
+	echo "[ROSLYN]" >> $LOG_FILE
+	cd $BASE_PATH/roslyn
+	$TIME make
+	echo "build result $?" >> $LOG_FILE
+fi
