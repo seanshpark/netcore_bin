9f4d2e0971d917967ad869dbb92f9bb19915462f
Sung-Jae Lee <sjlee@mail.com>
2016-04-30 10:29:46 +0900

    support alternate path searching for OpenGrok.

--- update-index.sh
+++ update-index.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-#OPENGROK=/opt/local/opengrok/bin/OpenGrok
 OPENGROK=/opt/opengrok/bin/OpenGrok
+if ! [ -x $OPENGROK ]; then
+	OPENGROK=/opt/local/opengrok/bin/OpenGrok
+fi
 
 for repo in `find . -maxdepth 1 -type d`; do
 	if [ "$repo" != "." ]; then
