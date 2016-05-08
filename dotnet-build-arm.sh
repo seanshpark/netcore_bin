8c4ea4ab01cf39fa0a113f27a8d6342053f8033c
Sung-Jae Lee <sjlee@mail.com>
2016-05-08 18:33:47 +0900

    remove old scripts

--- dotnet-build-arm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ~/git/coreclr
-ROOTFS_DIR=~/arm-rootfs-coreclr/ time ./build.sh clean arm cross verbose
-cd ~/git/corefx
-ROOTFS_DIR=~/arm-rootfs-corefx/ time ./build.sh clean arm cross verbose native skiptests
-time ./build.sh clean verbose skiptests
-#cd ~/git/coreclr
-#ROOTFS_DIR=~/arm-softfp-rootfs-coreclr/ time ./build.sh clean arm-softfp cross verbose
