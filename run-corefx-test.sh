acaa21b75ade8d249aadd2756ab7e2f42b8b56df
Sung-Jae Lee <sjlee@mail.com>
2016-07-07 21:45:29 +0900

    Add script for running corefx text.

---
+++ run-corefx-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ -z "$BASE_PATH" ]; then
+    BASE_PATH=$(pwd)
+fi
+
+LOG_FILE="$BASE_PATH/$(basename ${0}).log"
+
+cd ~/git/corefx
+./run-test.sh \
+--ignore-native-dlls \
+--sequential \
+--coreclr-bins /home/sjlee/git/coreclr/bin/Product/Linux.arm.Debug \
+--mscorlib-bins /home/sjlee/git/coreclr/bin/Product/Linux.arm.Debug \
+--corefx-tests /home/sjlee/git/corefx/bin/tests \
+--corefx-native-bins /home/sjlee/git/corefx/bin/Linux.arm.Debug/Native \
+--corefx-packages /home/sjlee/git/corefx/packages \
+| tee $LOG_FILE
