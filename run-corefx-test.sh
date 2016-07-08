79474b4553e3b74da596e9d4ba48ae65a5695f8e
Sung-Jae Lee <sjlee@mail.com>
2016-07-08 20:20:37 +0900

    run-corefx-tests.sh, change defaul option.

--- run-corefx-test.sh
+++ run-corefx-test.sh
@@ -8,11 +8,10 @@ LOG_FILE="$BASE_PATH/$(basename ${0}).log"
 
 cd ~/git/corefx
 ./run-test.sh \
---ignore-native-dlls \
 --sequential \
 --coreclr-bins /home/sjlee/git/coreclr/bin/Product/Linux.arm.Debug \
 --mscorlib-bins /home/sjlee/git/coreclr/bin/Product/Linux.arm.Debug \
 --corefx-tests /home/sjlee/git/corefx/bin/tests \
 --corefx-native-bins /home/sjlee/git/corefx/bin/Linux.arm.Debug/Native \
 --corefx-packages /home/sjlee/git/corefx/packages \
-| tee $LOG_FILE
+|& tee $LOG_FILE
