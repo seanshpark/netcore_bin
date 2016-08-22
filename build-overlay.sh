3bc35f9d4dba40fa3733e85e0051c850cd58e512
Sung-Jae Lee <sjlee@mail.com>
2016-08-22 20:02:10 +0900

    Add scripts
    * coreclr and corefx build script for host target.
    * script for building dotnet-overlay from coreclr and corefx build result.

---
+++ build-overlay.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+function print_usage 
+{
+    echo ''
+    echo "Usage: [BASE_PATH=<path>] $(basename $0) <OS.ARCH.BUILD> [options]"
+    echo ''
+    echo 'Typical sample:'
+    echo '  --coreClrBinDir="coreclr/bin/Product/Linux.x64.Debug"'
+    echo '  --coreFxBinDir="corefx/bin/Linux.AnyCPU.Debug;corefx/bin/Unix.AnyCPU.Debug;corefx/bin/AnyOS.AnyCPU.Debug"'
+    echo '  --coreFxNativeBinDir="corefx/bin/Linux.x64.Debug"'
+    echo ''
+    echo 'Optional arguments:'
+    echo '  --coreClrBinDir=<path>           : Directory of the CoreCLR build.'
+    echo '  --coreFxBinDir="<path>[;<path>]" : List of one or more directories with CoreFX build outputs (semicolon-delimited)'
+    echo '                                     If files with the same name are present in multiple directories, the first one wins.'
+    echo '  --coreFxNativeBinDir=<path>      : Directory of the CoreFX native build.'
+    echo '  --mscorlibDir=<path>             : Directory containing the built mscorlib.dll. If not specified, it is expected to be'
+    echo '                                     in the directory specified by --coreClrBinDir.'
+    echo ''
+    echo '  --crossgen                       : Precompile any assembly to NI image.'
+    echo ''
+}
+
+TARGET=(${1//./ })
+
+OS=${TARGET[0]}
+ARCHITECTURE=${TARGET[1]}
+BUILD=${TARGET[2]}
+shift
+
+if [ -z "$OS" ] || [ -z "$ARCHITECTURE" ] || [ -z "$BUILD" ]
+then
+    print_usage
+    exit 0
+else
+    echo ""
+    echo "OS=$OS"
+    echo "ARCHITECTURE=$ARCHITECTURE"
+    echo "BUILD=$BUILD"
+    echo ""
+fi
+
+if [ -z "$BASE_PATH" ]; then
+    BASE_PATH=$(pwd)
+fi
+
+# libExtension determines extension for dynamic library files
+OSName=$(uname -s)
+libExtension=
+case $OSName in
+    Darwin)
+        libExtension="dylib"
+        ;;
+    Linux|NetBSD)
+        libExtension="so"
+        ;;
+    *)
+        echo "Unsupported OS $OSName detected, configuring as if for Linux"
+        libExtension="so"
+        ;;
+esac
+
+function create_core_overlay 
+{
+    if [ -z "$coreClrBinDir" ] || [ ! -d "$coreClrBinDir" ]; then
+        echo "ERROR: --coreClrBinDir must be specified & exist."
+        exit 1
+    fi
+    if [ ! -f "$mscorlibDir/mscorlib.dll" ]; then
+        echo "ERROR: mscorlib.dll was not found in: $mscorlibDir"
+        exit 1
+    fi
+    if [ -z "$coreFxBinDir" ]; then
+        echo "ERROR: --coreFxBinDir must be specified."
+        exit 1
+    fi
+    if [ -z "$coreFxNativeBinDir" ]; then
+        echo "ERROR: --coreFxNativeBinDir must be specified."
+        exit 1
+    fi
+    if [ ! -d "$coreFxNativeBinDir/Native" ]; then
+        echo "ERROR: Directory specified by --coreNativeFxBinDir does not exist: $coreFxNativeBinDir/Native"
+        exit 1
+    fi
+
+    # Create the overlay
+    if [ -e "$coreOverlayDir" ]; then
+        rm -f -r "$coreOverlayDir"
+    fi
+    mkdir "$coreOverlayDir"
+
+    while IFS=';' read -ra coreFxBinDirectories
+    do
+        for currDir in "${coreFxBinDirectories[@]}"
+        do
+            if [ ! -d "$currDir" ]
+            then
+                echo "ERROR: Directory specified in --coreFxBinDir does not exist: $currDir"
+            fi
+            pushd $currDir > /dev/null
+            for dirName in $(find . -iname '*.dll' \! -iwholename '*test*' \! -iwholename '*/ToolRuntime/*' \! -iwholename '*/RemoteExecutorConsoleApp/*' \! -iwholename '*/net*' \! -iwholename '*aot*' -exec dirname {} \; | uniq | sed 's/\.\/\(.*\)/\1/g')
+            do
+                #cp -n -v "$currDir/$dirName/$dirName.dll" "$coreOverlayDir/"
+                cp -n -v "$dirName/$dirName.dll" "$coreOverlayDir/"
+            done
+            popd $currDur > /dev/null
+        done
+    done <<< $coreFxBinDir
+
+    cp -f -v "$coreFxNativeBinDir/Native/"*."$libExtension" "$coreOverlayDir/" 2>/dev/null
+    cp -f -v "$coreClrBinDir/"* "$coreOverlayDir/" 2>/dev/null
+    #    cp -f -v "$coreClrBinDir/bin/"* "$coreOverlayDir/" 2>/dev/null
+    cp -f -v "$mscorlibDir/mscorlib.dll" "$coreOverlayDir/"
+
+    #    cp -n -v "$testDependenciesDir"/* "$coreOverlayDir/" 2>/dev/null
+    #    if [ -f "$coreOverlayDir/mscorlib.ni.dll" ]; then
+    #        # Test dependencies come from a Windows build, and mscorlib.ni.dll would be the one from Windows
+    #        rm -f "$coreOverlayDir/mscorlib.ni.dll"
+    #    fi
+}
+
+function precompile_overlay_assemblies 
+{
+    filesToPrecompile=$(ls -trh $overlayDir/*.dll)
+    for fileToPrecompile in ${filesToPrecompile}
+    do
+        local filename=${fileToPrecompile}
+        # Precompile any assembly except mscorlib since we already have its NI image available.
+        if [[ "$filename" != *"mscorlib.dll"* ]]; then
+            if [[ "$filename" != *"mscorlib.ni.dll"* ]]; then
+                echo Precompiling $filename
+                $overlayDir/crossgen /Platform_Assemblies_Paths $overlayDir $filename 2>/dev/null
+                local exitCode=$?
+                if [ $exitCode == -2146230517 ]; then
+                    echo $filename is not a managed assembly.
+                elif [ $exitCode != 0 ]; then
+                    echo Unable to precompile $filename.
+                else
+                    echo Successfully precompiled $filename
+                fi
+            fi
+        fi
+    done
+}
+
+coreOverlayDir="${BASE_PATH}/dotnet-overlay"
+coreClrBinDir="coreclr/bin/Product/${OS}.${ARCHITECTURE}.${BUILD}"
+mscorlibDir="coreclr/bin/Product/${OS}.${ARCHITECTURE}.${BUILD}"
+coreFxBinDir="corefx/bin/${OS}.${ARCHITECTURE}.${BUILD};corefx/bin/${OS}.AnyCPU.${BUILD};corefx/bin/Unix.AnyCPU.${BUILD};corefx/bin/AnyOS.AnyCPU.${BUILD};"
+coreFxNativeBinDir="corefx/bin/${OS}.${ARCHITECTURE}.${BUILD}"
+
+for i in "$@"
+do
+    case $i in
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        --crossgen)
+            precompile_overlay_assemblies 
+            exit 0;
+            ;;
+        --coreClrBinDir=*)
+            coreClrBinDir=${i#*=}
+            ;;
+        --mscorlibDir=*)
+            mscorlibDir=${i#*=}
+            ;;
+        --coreFxBinDir=*)
+            coreFxBinDir=${i#*=}
+            ;;
+        --coreFxNativeBinDir=*)
+            coreFxNativeBinDir=${i#*=}
+            ;;
+        *)
+            print_usage
+            exit 0
+            ;;
+    esac
+done
+
+if [ -z "$mscorlibDir" ]; then
+    mscorlibDir=$coreClrBinDir
+fi
+
+create_core_overlay
+
+exit 0
---
+++ dotnet-build-host.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+#
+# Written by Sung-Jae Lee (sjlee@mail.com)
+#
+
+function usage
+{
+    echo ''
+    echo "Usage: [ENV=<value>] $(basename $0) [option]"
+    echo ''
+    echo '    [ENV=<value>]'
+    echo "       [BASE_PATH=<git base>]"
+    echo ''
+    echo '    [option]'
+    echo '        -? | -h | --help : Print this instruction.'
+    echo '                   clean : Do clean build.'
+    echo '                 verbose : Verbose output.'
+    echo '                   debug : Build Debug configuration.'
+    echo '                 release : Build Release configuration.'
+    echo '                     all : Build all.'
+    echo '                 coreclr : Build CoreCLR.'
+    echo '                  corefx : Build CoreFX.'
+    echo '           corefx-native : Build CoreFX native only.'
+    echo '          corefx-managed : Build CoreFX managed only.'
+    echo ''
+    echo '            --skip-build : Skip build.'
+    echo '      --enable-jit-debug : Enable JIT code debugging with lldb + libsos.dll.'
+    echo '     --copy-overlay-only : Don'"'"'t copy full CoreCLR test package. Instead, copy coreoverlay directory only.'
+    echo '             --outerloop : Build corefx outloop test.'
+    echo ''
+    exit
+}
+
+# set $BASE_PATH
+if [ -z "$BASE_PATH" ]; then
+    BASE_PATH=$(pwd)
+fi
+
+# set $TARGET_DEVICE
+if [ -z "$TARGET_DEVICE" ]; then
+    TARGET_DEVICE=pi2
+fi
+
+# initialize configuration variable
+COMMAND_LINE="$(basename $0) $*"
+CONFIGURATION=
+CLEAN_BUILD=0
+VERBOSE=
+SKIP_BUILD=0
+EXTRA_OPTIONS=
+TOTAL_RESULT=
+TOTAL_EXIT_CODE=0
+TIME=
+ENABLE_JIT_DEBUG=
+OUTERLOOP=
+BUILD_CORECLR=0
+BUILD_COREFX_NATIVE=0
+BUILD_COREFX_MANAGED=0
+
+function message
+{
+    echo ''
+    echo $1
+    echo ''
+}
+
+# define functions
+function check_result
+{
+    local RESULT=$1
+    local EXIT_CODE=$2
+
+    TOTAL_RESULT="${TOTAL_RESULT}${RESULT}"
+    message "[BUILD RESULT: $RESULT]"
+
+    if [ "$RESULT" -ne "0" ]
+    then
+        let "TOTAL_EXIT_CODE+=$EXIT_CODE"
+        message "[SCRIPT STOPPED WITH $TOTAL_EXIT_CODE ($TOTAL_RESULT)]"
+        exit $TOTAL_EXIT_CODE
+    fi
+}
+
+function do_clean
+{
+    if [ "$CLEAN_BUILD" == "1" ]
+    then
+        message "[CLEAN $1]"
+        ./clean.sh
+    fi
+}
+
+if [ $# == 0 ]
+then
+    usage
+    exit
+fi
+
+# check commandline options
+while [ -n "$1" ]
+do
+    case $1 in
+        -?|-h|--help)
+            usage
+            ;;
+        clean)
+            CLEAN_BUILD=1
+            ;;
+        verbose)
+            VERBOSE=verbose
+            ;;
+        release|debug)
+            CONFIGURATION=$1
+            ;;
+        all)
+            BUILD_CORECLR=1
+            BUILD_COREFX_NATIVE=1
+            BUILD_COREFX_MANAGED=1
+            ;;
+        coreclr)
+            BUILD_CORECLR=1
+            ;;
+        corefx)
+            BUILD_COREFX_NATIVE=1
+            BUILD_COREFX_MANAGED=1
+            ;;
+        corefx-native)
+            BUILD_COREFX_NATIVE=1
+            ;;
+        corefx-managed)
+            BUILD_COREFX_MANAGED=1
+            ;;
+        --skip-build)
+            SKIP_BUILD=1
+            ;;
+        --enable-jit-debug)
+            ENABLE_JIT_DEBUG="cmakeargs -DFEATURE_GDBJIT=TRUE"
+            ;;
+        --outerloop)
+            OUTERLOOP="/p:Outerloop=true"
+            ;;
+        *)
+            EXTRA_OPTIONS="${EXTRA_OPTIONS} $1"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$CONFIGURATION" ]
+then
+    echo "ERROR: need build configuration."
+    exit 1
+fi
+
+if [ "$BUILD_CORECLR" == "0" ] && [ "$BUILD_COREFX_NATIVE" == "0" ] && [ "$BUILD_COREFX_MANAGED" == "0" ]
+then
+    SKIP_BUILD=1
+fi
+
+# initialize variable
+CONFIGURATION=$(echo $CONFIGURATION | tr '[:upper:]' '[:lower:]')
+DATETIME=$(date +%Y%m%d-%T)
+
+echo "$COMMAND_LINE"
+echo ''
+echo "CONFIGURATION=$(echo $CONFIGURATION | tr '[:upper:]' '[:lower:]')"
+echo "DATETIME=$(date +%Y%m%d-%T)"
+echo ''
+
+# build coreclr
+if [ "$SKIP_BUILD" != "1" ]  && [ "$BUILD_CORECLR" == "1" ]
+then
+    cd $BASE_PATH/coreclr
+    do_clean "CORECLR"
+
+    message "[BUILD CORECLR]"
+    echo "$TIME ./build.sh $CONFIGURATION $VERBOSE $ENABLE_JIT_DEBUG clang3.8 |& tee $BASE_PATH/coreclr-build-${DATETIME}.log" | tee $BASE_PATH/coreclr-build-${DATETIME}.log
+    $TIME ./build.sh $CONFIGURATION $VERBOSE $ENABLE_JIT_DEBUG clang3.8 |& tee -a $BASE_PATH/coreclr-build-${DATETIME}.log
+    RESULT=$?
+    check_result $RESULT 1
+fi
+
+# build corefx
+if [ "$SKIP_BUILD" != "1" ]
+then
+    if [ "$BUILD_COREFX_NATIVE" == "1" ] || [ "$BUILD_COREFX_MANAGED" == "1" ]
+    then
+        cd $BASE_PATH/corefx
+        do_clean "COREFX"
+    fi
+
+    if [ "$BUILD_COREFX_NATIVE" == "1" ]
+    then
+        message "[BUILD COREFX-NATIVE]"
+        echo "$TIME ./build-native.sh -$CONFIGURATION -- /p:SkipTests=true $VERBOSE $OUTERLOOP /p:TestWithoutNativeImages=true |& tee $BASE_PATH/corefx-native-build-${DATETIME}.log" | tee $BASE_PATH/corefx-native-build-${DATETIME}.log
+        $TIME ./build-native.sh -$CONFIGURATION -- /p:SkipTests=true $VERBOSE $OUTERLOOP /p:TestWithoutNativeImages=true |& tee -a $BASE_PATH/corefx-native-build-${DATETIME}.log
+        RESULT=$?
+        check_result $RESULT 2
+    fi
+
+    if [ "$BUILD_COREFX_MANAGED" == "1" ]
+    then
+        message "[BUILD COREFX-MANAGED]"
+        echo "$TIME ./build-managed.sh -$CONFIGURATION -- /p:SkipTests=true $OUTERLOOP /p:TestWithoutNativeImages=true |& tee $BASE_PATH/corefx-managed-build-${DATETIME}.log" | tee $BASE_PATH/corefx-managed-build-${DATETIME}.log
+        $TIME ./build-managed.sh -$CONFIGURATION -- /p:SkipTests=true $OUTERLOOP /p:TestWithoutNativeImages=true |& tee -a $BASE_PATH/corefx-managed-build-${DATETIME}.log
+        RESULT=$?
+        check_result $RESULT 4
+    fi
+fi
+
+if [ "$TOTAL_EXIT_CODE" -ne "0" ]
+then
+    message "[SCRIPT STOPPED WITH $TOTAL_EXIT_CODE ($TOTAL_RESULT)]"
+    exit $TOTAL_EXIT_CODE
+fi
+
+# notify
+if [ -n "$NOTIFY" ]; then
+    message "$NOTIFY \"[$(hostname -s)] $COMMAND_LINE [$TOTAL_RESULT] $(date '+%D %T')\""
+    $NOTIFY "[$(hostname -s)] $COMMAND_LINE [$TOTAL_RESULT] $(date '+%D %T')"
+fi
+
+#
+# | Example of setting $NOTIFY in `~/.profile` |
+#
+# export NOTIFY="twidge dmsend lemmaa"
+#
