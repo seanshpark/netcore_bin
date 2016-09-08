794832bbf6fd7dac9e972e60cf1be52b8bb7326e
Sung-Jae Lee <sjlee@mail.com>
2016-09-08 09:14:40 +0900

    Auto detect test target platform.

--- corefx-test-mono/run-corefx-test-mono.sh
+++ corefx-test-mono/run-corefx-test-mono.sh
@@ -5,9 +5,43 @@ export UNW_ARM_UNWIND_METHOD=6
 function usage
 {
     echo ''
-    echo "Usage: [CORECLR_BINS=./coreclr/bin/Product/Linux.arm.Release] $(basename $0) {Debug|Release}"
+    echo "Usage: [CORECLR_BINS=./coreclr/bin/Product/Linux.${ARCH}.Release] $(basename $0) {Debug|Release}"
     echo ''
 }
+
+function check_cpu_architecture 
+{
+    local CPUName=$(uname -p)
+    local __arch=
+
+    # Some Linux platforms report unknown for platform, but the arch for machine.
+    if [ "$CPUName" == "unknown" ]; then
+        CPUName=$(uname -m)
+    fi
+
+    case $CPUName in
+        i686)
+            __arch=x86
+            ;;
+        x86_64)
+            __arch=x64
+            ;;
+        armv7l)
+            __arch=arm
+            ;;
+        aarch64)
+            __arch=arm64
+            ;;
+        *)
+            echo "Unknown CPU $CPUName detected, configuring as if for x64"
+            __arch=x64
+            ;;
+    esac
+
+    echo "$__arch"
+}
+
+ARCH=$(check_cpu_architecture)
 
 if [ $# -eq 0 ]; then
     usage
@@ -36,4 +70,4 @@ done
 
 BASE_PATH=$(pwd)
 
-./run-test-mono.sh --sequential --configurationGroup ${TEST_CONFIGURATION} --coreclr-bins $BASE_PATH/coreclr/bin/Product/Linux.arm.Release --mscorlib-bins $BASE_PATH/coreclr/bin/Product/Linux.arm.Release --corefx-tests $BASE_PATH/corefx/bin/tests --corefx-native-bins $BASE_PATH/corefx/bin/Linux.arm.Release/Native --corefx-packages $BASE_PATH/corefx/packages $EXTRA_OPTIONS | tee $BASE_PATH/corefx-Linux.arm.Release-test.log
+./run-test-mono.sh --sequential --configurationGroup ${TEST_CONFIGURATION} --coreclr-bins $BASE_PATH/coreclr/bin/Product/Linux.${ARCH}.Release --mscorlib-bins $BASE_PATH/coreclr/bin/Product/Linux.${ARCH}.Release --corefx-tests $BASE_PATH/corefx/bin/tests --corefx-native-bins $BASE_PATH/corefx/bin/Linux.${ARCH}.Release/Native --corefx-packages $BASE_PATH/corefx/packages $EXTRA_OPTIONS | tee $BASE_PATH/corefx-Linux.${ARCH}.Release-test.log
