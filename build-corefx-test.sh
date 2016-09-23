6962d73cdd51654cab7313f8598378ed89021420
Sung-Jae Lee <sjlee@mail.com>
2016-09-23 20:34:54 +0900

    Fix pipe error to log stderr also.

--- build-corefx-test.sh
+++ build-corefx-test.sh
@@ -96,7 +96,7 @@ BASE_PATH=\$(pwd)
 --corefx-native-bins \$BASE_PATH/corefx/bin/${OS}.${ARCHITECTURE}.${BUILD}/Native \
 --corefx-packages \$BASE_PATH/corefx/packages \
 \$EXTRA_OPTIONS \
-| tee \$BASE_PATH/$(basename $TEST_ROOT).log
+|& tee \$BASE_PATH/$(basename $TEST_ROOT).log
 END
 
     chmod 775 $TEST_SCRIPT
